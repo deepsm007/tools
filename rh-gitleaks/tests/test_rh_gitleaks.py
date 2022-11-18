d688997f381df74e397aa5c999f0a9d8ee76a650
Braxton Plaxco <bplaxco@redhat.com>
2022-11-18 13:02:41 -0500

    Add more test cases

--- rh-gitleaks/tests/test_rh_gitleaks.py
+++ rh-gitleaks/tests/test_rh_gitleaks.py
@@ -13,7 +13,6 @@ import rh_gitleaks
 class RHGitleaksTest(TestCase):
     mock_resp = b"mock"
     mock_sha256sum = hashlib.sha256(mock_resp).hexdigest()
-
     mock_token = jwt.encode(
         {
             "iss": "pattern-distribution-server-mock",
@@ -124,6 +123,30 @@ class RHGitleaksTest(TestCase):
             self.assertEqual(rh_gitleaks.jwt_valid(token), expected, f"test={i}")
 
     @patch("requests.get")
+    def test_configure(self, mock_requests_get):
+        """
+        Test configure
+        """
+        mock_requests_get.return_value.content = self.mock_resp
+        rh_gitleaks.config.GITLEAKS_BIN_DOWNLOAD_SHA256SUM = self.mock_sha256sum
+
+        with TemporaryDirectory() as tmp_dir:
+            # The extra dir in the middle is to confirm it will be created
+            # if it doesn't exist
+            rh_gitleaks.config.PATTERNS_AUTH_JWT_PATH = os.path.join(
+                tmp_dir, "a", "auth.jwt"
+            )
+
+            # Make sure it exits cleanly and loads the token
+            self.assertEqual(rh_gitleaks.load_auth_token(), None)
+            self.assertEqual(rh_gitleaks.configure(auth_jwt=self.mock_token), 0)
+            self.assertEqual(rh_gitleaks.load_auth_token(), self.mock_token)
+
+            # Make sure it can be ran twice
+            self.assertEqual(rh_gitleaks.configure(auth_jwt=self.mock_token), 0)
+            self.assertEqual(rh_gitleaks.load_auth_token(), self.mock_token)
+
+    @patch("requests.get")
     @patch("subprocess.run")
     def test_run_gitleaks(self, mock_subprocess_run, mock_requests_get):
         """
@@ -147,6 +170,7 @@ class RHGitleaksTest(TestCase):
             )
 
             # Check the results of a configure and run
+            self.assertEqual(rh_gitleaks.run_gitleaks(["--help"]), 1)
             self.assertEqual(rh_gitleaks.configure(auth_jwt=self.mock_token), 0)
             self.assertEqual(rh_gitleaks.run_gitleaks(["--help"]), 0)
             self.assertEqual(
