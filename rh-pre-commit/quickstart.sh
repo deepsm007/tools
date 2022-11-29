21502cd1f3789ffb939de4df24057effdee47515
Braxton Plaxco <bplaxco@redhat.com>
2022-11-29 23:00:31 +0000

    Add banner

--- rh-pre-commit/quickstart.sh
+++ rh-pre-commit/quickstart.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/bash
 set -xeuo pipefail
 
+#
+# WARNING: This will apply rh-multi-pre-commit to every
+# repo in your home dir and overwrite any existing pre-commit
+# hooks
+#
+
 # Pull a fresh copy of the repo
 rm -rf /tmp/infosec-tools
 git clone https://gitlab.corp.redhat.com/infosec-public/developer-workbench/tools.git /tmp/infosec-tools
