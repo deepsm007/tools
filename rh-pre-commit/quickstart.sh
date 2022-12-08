4818fa07c03ea56928e20cf92c7b0e501919c4a6
Braxton Plaxco <bplaxco@redhat.com>
2022-12-08 08:19:34 -0500

    Improve quickstart
    * Clean it up
    * Allow it to optionally take a branch argument

--- rh-pre-commit/quickstart.sh
+++ rh-pre-commit/quickstart.sh
@@ -1,18 +1,42 @@
 #! /usr/bin/bash
-set -xeuo pipefail
-
 #
-# WARNING: This will apply rh-multi-pre-commit to every
-# repo in your home dir and overwrite any existing pre-commit
-# hooks
+# WARNING
 #
+#   This will apply rh-multi-pre-commit to every repo in your home dir and
+#   overwrite any existing pre-commit hooks!
+#
+# USAGE
+#
+#   ./quickstart.sh [branch]
+#
+# OPTIONS
+#
+#   branch - (optional) which branch should be installed
+#
+set -xeo pipefail
 
 # Pull a fresh copy of the repo
 rm -rf /tmp/infosec-tools
 git clone https://gitlab.corp.redhat.com/infosec-public/developer-workbench/tools.git /tmp/infosec-tools
 cd /tmp/infosec-tools/rh-pre-commit
 
-# Install and configure the tools with the default settings
+# Checkout a branch if it was specified
+if [[ ! -z "$1" ]]
+then
+  git checkout $1
+fi
+
+# Upgrade pip
+python3 -m pip install --upgrade --user pip
+
+# Uninstall any legacy versions of the script if present
+python3 ../scripts/uninstall-legacy-tools
+
+# Install the tools
 make install
-rh-multi-pre-commit configure --configure-git-template --force
-rh-multi-pre-commit install --force --path ~/
+
+# Configure it with the default settings
+python3 -m rh_pre_commit.multi configure --configure-git-template --force
+
+# Enable it for all existing projects under the home directory
+python3 -m rh_pre_commit.multi install --force --path "${HOME}"
